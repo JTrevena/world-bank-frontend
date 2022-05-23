4f455fa71c8a2bbff9bc585cc1c308ab70661dfc
Ibahmed1 <ib.ahmed1@outlook.com>
2022-05-23 17:15:29 +0100

    setup for everyone to start working

---
+++ world-bank-dashboard/src/SignupPage/Signup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Signup(props) {
+  return <div></div>;
+}
--- world-bank-dashboard/src/index.js
+++ world-bank-dashboard/src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./Home/Home.js";
+import Login from "./LoginPage/Login";
+import Signup from "./SignupPage/Signup";
+import History from "./HistoryPage/History";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
