9e54b4209699aea5979b19331e5db888d9b62f89
Ibahmed1 <ib.ahmed1@outlook.com>
2022-05-24 12:38:01 +0100

    created create account page with functionality

--- world-bank-dashboard/src/Networking.js
+++ world-bank-dashboard/src/Networking.js
@@ -1,3 +1,15 @@
 export default class Networking {
   //try and write any fetch request functions in here
+  async createAccount(username, password) {
+    const response = await fetch("/create-user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }); // waiting for for backend endpoint to be finalised. This is a placeholder
+  }
 }
