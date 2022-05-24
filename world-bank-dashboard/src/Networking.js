ae4d281ec3c97cd0b6bf45bea2385a68375b9e8e
kainanh <kainanh@hotmail.co.uk>
2022-05-24 13:08:50 +0100

    login functionality

--- world-bank-dashboard/src/Networking.js
+++ world-bank-dashboard/src/Networking.js
@@ -1,3 +1,14 @@
 export default class Networking {
   //try and write any fetch request functions in here
+  async postUser(username, password) {
+    const newUser = { username, password };
+
+    await fetch(`/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    });
+  }
 }
