12eeb622a2f4a3f115853dac45cc8ed55e14f0d0
kainanh <kainanh@hotmail.co.uk>
2022-05-24 16:50:10 +0100

    Search page components and functionality

---
+++ world-bank-dashboard/src/Home/Search.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import "./Home.css";
+import {
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import Networking from "../Networking";
+
+export default function Search(props) {
+  const [startYear, setStartYear] = useState("");
+  const [endYear, setEndYear] = useState("");
+  const [country, setCountry] = useState("");
+  const [indicator, setIndicator] = useState("");
+
+  const networking = new Networking();
+
+  function yearButtons() {
+    let years = [];
+    for (let i = 1960; i < 2023; i++) {
+      years.unshift(i);
+    }
+    return years.map((year) => {
+      return <MenuItem value={String(year)}>{String(year)}</MenuItem>;
+    });
+  }
+
+  function handleSearch() {
+    networking.searchQuery(country, indicator, startYear, endYear);
+  }
+  return (
+    <div className="main-wrapper">
+      <div className="search-form-wrapper">
+        <form id="search-query">
+          <TextField
+            id="outlined-basic"
+            label="Country"
+            variant="outlined"
+            helperText="Please enter your country"
+            onChange={(e) => setCountry(e.target.value)}
+            // InputLabelProps={{
+            //   style: { color: "white" },
+            // }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Indicators"
+            variant="outlined"
+            helperText="Please enter your indicators"
+            onChange={(e) => setIndicator(e.target.value)}
+            // InputLabelProps={{
+            //   style: { color: "white" },
+            // }}
+          />
+          <FormControl sx={{ minWidth: 100 }}>
+            <InputLabel id="start-year">Year</InputLabel>
+            <Select
+              labelId="start-year"
+              id="start-year"
+              label="Year"
+              onChange={(e) => setStartYear(e.target.value)}
+              defaultValue=""
+            >
+              {yearButtons()}
+            </Select>
+          </FormControl>
+          <FormControl sx={{ minWidth: 100 }}>
+            <InputLabel id="end-year">Year</InputLabel>
+            <Select
+              labelId="end-year"
+              id="end-year"
+              label="Year"
+              onChange={(e) => setEndYear(e.target.value)}
+              defaultValue=""
+            >
+              {yearButtons()}
+            </Select>
+          </FormControl>
+        </form>
+      </div>
+      <div className="search-form-wrapper">
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+      </div>
+    </div>
+  );
+}
--- world-bank-dashboard/src/Networking.js
+++ world-bank-dashboard/src/Networking.js
@@ -1,14 +1,28 @@
 export default class Networking {
   //try and write any fetch request functions in here
   async postUser(username, password) {
-    const newUser = { username, password };
+    const userLoginDetails = { username, password };
 
-    await fetch(`/login`, {
+    await fetch(`https://safe-harbor-88927.herokuapp.com/login`, {
       method: "POST",
+      credentials: "include",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(newUser),
+      body: JSON.stringify(userLoginDetails),
+    });
+  }
+  async searchQuery(country, indicator, startYear, endYear) {
+    let url = `https://safe-harbor-88927.herokuapp.com/results?country=${country}`;
+    if (indicator) url += `&indicator=${indicator}`;
+    if (startYear) url += `&startYear=${startYear}`;
+    if (endYear) url += `&endYear=${endYear}`;
+    const response = await fetch(url, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json,",
+      },
     });
   }
 }
