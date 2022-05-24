90fe6259a4949723be361bb5c8b606ff076b8c87
Ibahmed1 <ib.ahmed1@outlook.com>
2022-05-24 14:06:06 +0100

    changed variable name in Networking createAccount function

--- world-bank-dashboard/src/Networking.js
+++ world-bank-dashboard/src/Networking.js
@@ -1,15 +1,14 @@
 export default class Networking {
   //try and write any fetch request functions in here
   async createAccount(username, password) {
+    const newUser = { username, password };
+
     const response = await fetch("/create-user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
+      body: JSON.stringify(newUser),
     }); // waiting for for backend endpoint to be finalised. This is a placeholder
   }
 }
